90f1e61eab4dcb576b4b9d2bc522e3d2c39f3bc1
Muthu4558 <muthukdm45@gmail.com>
2026-04-28 15:56:05 +0530

    UI done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     const [password, setPassword] = useState("");
 
     // ✅ LOGIN FUNCTION
-    const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+    const API_URL = import.meta.env.VITE_API_URL;
 
     const handleLogin = async () => {
         try {
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -15,7 +15,7 @@ const Signup = () => {
   const [partnerCode, setPartnerCode] = useState("");
 
   // ✅ REGISTER FUNCTION
-  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+  const API_URL = import.meta.env.VITE_API_URL;
 
   const handleSignup = async () => {
     try {
