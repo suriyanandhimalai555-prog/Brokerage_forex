92eef24b1ea67f5ae627bb9639dad0aca40c4f95
Muthu4558 <muthukdm45@gmail.com>
2026-04-28 16:00:38 +0530

    login signup done

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,9 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "AVG_Forex",
-  password: "Muthu@45",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: String(process.env.DB_PASS), // ✅ FORCE STRING
+  port: Number(process.env.DB_PORT) || 5432,
 });
